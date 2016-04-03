c0dc0c7ca63222abd532f5c18ef82a79ba436047
Jivan517 <np.jiv004@gmail.com>
2016-04-03 16:57:39 -0500

    ArtistDAO and DirectorDAO added

---
+++ src/main/java/cs/mum/edu/extraCredit/dao/ArtistDAO.java
@@ -0,0 +1,124 @@
+package cs.mum.edu.extraCredit.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import cs.mum.edu.extraCredit.domain.Artist;
+
+public class ArtistDAO {
+
+	public void createArtist(Artist artist) throws Exception {
+		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+		Transaction tx = null;
+
+		try {
+			
+			tx = session.beginTransaction();
+			session.persist(artist);
+			tx.commit();
+			
+		} catch (Exception e) {
+
+			if(tx != null) tx.rollback();
+			e.printStackTrace();
+			throw new Exception(e);
+		}
+	}
+	
+	public Artist getArtist(int id) throws Exception{
+		
+		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+		Transaction tx = null;
+
+		try {
+			
+			tx = session.beginTransaction();
+			Artist artist = session.get(Artist.class, id);  //eager fetch
+			tx.commit();
+			return artist;
+			
+		} catch (Exception e) {
+
+			if(tx != null) tx.rollback();
+			e.printStackTrace();
+			throw new Exception(e);
+		}
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<Artist> getArtistByName(String name) throws Exception{
+		
+		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+		Transaction tx = null;
+		List<Artist> artists = new ArrayList<>();
+		
+		try {
+			
+			tx = session.beginTransaction();
+			Query artistQuery = session.createQuery("FROM Artist m WHERE m.name = :name");
+			artistQuery.setParameter("name", name);
+			
+			artists = artistQuery.list();
+			
+			tx.commit();
+			
+		} catch (Exception e) {
+
+			if(tx != null) tx.rollback();
+			e.printStackTrace();
+			throw new Exception(e);
+		}
+		
+		return artists;
+		
+	}
+	
+	public void updateArtist(Artist artist) throws Exception{
+		
+		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+		Transaction tx = null;
+		
+		try {
+			
+			tx = session.beginTransaction();
+			
+			session.merge(artist);
+			
+			tx.commit();
+			
+		} catch (Exception e) {
+
+			if(tx != null) tx.rollback();
+			e.printStackTrace();
+			throw new Exception(e);
+			
+		}
+		
+	}
+	
+	public void deleteArtist(Artist artist)throws Exception{
+		
+		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+		Transaction tx = null;
+		
+		try {
+			
+			tx = session.beginTransaction();
+			
+			session.delete(artist);
+			
+			tx.commit();
+			
+		} catch (Exception e) {
+
+			if(tx != null) tx.rollback();
+			e.printStackTrace();
+			throw new Exception(e);
+			
+		}
+	}
+}
---
+++ src/main/java/cs/mum/edu/extraCredit/dao/DirectorDAO.java
@@ -0,0 +1,125 @@
+package cs.mum.edu.extraCredit.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import cs.mum.edu.extraCredit.domain.Director;
+
+public class DirectorDAO {
+
+	public void createDirector(Director director) throws Exception {
+		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+		Transaction tx = null;
+
+		try {
+			
+			tx = session.beginTransaction();
+			session.persist(director);
+			tx.commit();
+			
+		} catch (Exception e) {
+
+			if(tx != null) tx.rollback();
+			e.printStackTrace();
+			throw new Exception(e);
+		}
+	}
+	
+	public Director getDirector(int id) throws Exception{
+		
+		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+		Transaction tx = null;
+
+		try {
+			
+			tx = session.beginTransaction();
+			Director director = session.get(Director.class, id);  //eager fetch
+			tx.commit();
+			return director;
+			
+		} catch (Exception e) {
+
+			if(tx != null) tx.rollback();
+			e.printStackTrace();
+			throw new Exception(e);
+		}
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<Director> getDirectorByName(String name) throws Exception{
+		
+		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+		Transaction tx = null;
+		List<Director> directors = new ArrayList<>();
+		
+		try {
+			
+			tx = session.beginTransaction();
+			Query directorQuery = session.createQuery("FROM Director m WHERE m.name = :name");
+			directorQuery.setParameter("name", name);
+			
+			directors = directorQuery.list();
+			
+			tx.commit();
+			
+		} catch (Exception e) {
+
+			if(tx != null) tx.rollback();
+			e.printStackTrace();
+			throw new Exception(e);
+		}
+		
+		return directors;
+		
+	}
+	
+	public void updateDirector(Director director) throws Exception{
+		
+		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+		Transaction tx = null;
+		
+		try {
+			
+			tx = session.beginTransaction();
+			
+			session.merge(director);
+			
+			tx.commit();
+			
+		} catch (Exception e) {
+
+			if(tx != null) tx.rollback();
+			e.printStackTrace();
+			throw new Exception(e);
+			
+		}
+		
+	}
+	
+	public void deleteDirector(Director director)throws Exception{
+		
+		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+		Transaction tx = null;
+		
+		try {
+			
+			tx = session.beginTransaction();
+			
+			session.delete(director);
+			
+			tx.commit();
+			
+		} catch (Exception e) {
+
+			if(tx != null) tx.rollback();
+			e.printStackTrace();
+			throw new Exception(e);
+			
+		}
+	}
+
+}
